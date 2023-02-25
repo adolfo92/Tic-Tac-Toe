48516014d9a2f7f2bf0347f68f0398d4634df83a
Adolfo92 <adolfo92dev@gmail.com>
2023-02-24 21:05:19 -0400

    Agrega funcion para revisar si alguien gano (Solo verificada la condicion de diagonales)

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function GameBoard() {
     if (board[row][column].getValue() !== "") return;
 
     board[row][column].setValue(player);
-    console.log(board[row][column].getValue());
   };
 
   const printBoard = () => {
@@ -31,7 +30,61 @@ function GameBoard() {
     });
   };
 
-  return { printBoard, setMark, getBoard };
+  const checkForWinning = (playerToken) => {
+    let win = false;
+
+    let inverseBoard = [[], [], []];
+
+    // Creo tabla inversa para poder filtrar en un solo forEach()
+    board.forEach((row) => {
+      for (let i = 0; i < 3; i++) {
+        inverseBoard[i].push(row[i]);
+      }
+    });
+
+    board.forEach((row) => {
+      // Logica para columnas llenas
+      const Line = row.filter((cell) => cell.getValue() === playerToken);
+
+      if (Line.length === 3) {
+        win = true;
+      }
+    });
+
+    inverseBoard.forEach((row) => {
+      // Logica para filas llenas
+      const Line = row.filter((cell) => cell.getValue() === playerToken);
+
+      if (Line.length === 3) {
+        win = true;
+      }
+    });
+
+    // Logica para diagonales
+
+    const midValue = board[1][1].getValue();
+
+    if (midValue === "") return;
+
+    // Diagonal principal
+    if (
+      board[0][0].getValue() === midValue &&
+      board[2][2].getValue() === midValue
+    ) {
+      win = true;
+    }
+    // Diagonal secundaria
+    if (
+      board[0][2].getValue() === midValue &&
+      board[2][0].getValue() === midValue
+    ) {
+      win = true;
+    }
+
+    return win;
+  };
+
+  return { printBoard, setMark, getBoard, checkForWinning };
 }
 
 function Cell() {
@@ -48,7 +101,7 @@ function Cell() {
   return { setValue, getValue };
 }
 
-function Controller(playerOne = "Player One", playerTwo = "Player Two") {
+function Controller(playerOne = "Player One", playerTwo = "Machine") {
   // Cosas inherentes a interaccion
   const board = GameBoard();
 
@@ -82,18 +135,29 @@ function Controller(playerOne = "Player One", playerTwo = "Player Two") {
 
   const newRound = () => {
     board.printBoard();
-    console.log(`Es el turno de ${activePlayer.name}`);
+    console.log(`Es el turno de [${activePlayer.name}]`);
   };
 
   const playRound = (column, row) => {
     board.setMark(activePlayer.token, column, row);
+    if (board.checkForWinning(activePlayer.token)) {
+      board.printBoard();
+      console.log(`Jugador [${activePlayer.name}] gano la partida`);
+      return;
+    }
     switchTurn();
     newRound();
   };
 
   newRound();
 
-  return { setToken, playRound, getActivePlayer };
+  return { setToken, playRound, getActivePlayer, getBoard: board.printBoard };
 }
 
 const game = Controller();
+
+game.playRound(0, 0);
+game.playRound(1, 2);
+game.playRound(0, 1);
+game.playRound(2, 2);
+game.playRound(0, 2);
